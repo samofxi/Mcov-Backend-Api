d481b3b68146fa9210a30397f79dc3e84ef9e24b
Salman <78206653+samofxi@users.noreply.github.com>
2022-01-08 16:10:52 +0100

    cookies

--- services/serives.js
+++ services/serives.js
@@ -26,7 +26,6 @@ exports.neuUser = async (req,res,next) =>{
         console.log(birthday);
         let olduser = await user.findOne({ Nachname: req.body.Nachname, Vorname: req.body.Vorname, email:req.body.email})
         if(!olduser){
-            
         try {
             const newuser= await user.create({
                 id: id,
@@ -65,18 +64,17 @@ exports.neuUser = async (req,res,next) =>{
                 subject: "Ihre Terminbestätigung MCOV Schnelltest", // Subject line
                 text: code, // plain text body
                 html: `
-                  <p>Sehr geehrter Frau/Herr ${req.body.Nachname},</p>
-                  <p>anbei erhalten Sie Ihre pers&ouml;nliche Registrierungsnummer f&uuml;r den COVID-19 Schnelltest. Bitte beachten Sie, dass diese Registrierungsnummer nur einmal verwendet werden kann.</p>
-                  <p>Enclosed you will receive your personal registration number for the COVID-19 rapid test. Please note that this registration number can only be used once.</p>
-                  <p>Ihre ID / your ID:</p>
-                  <p>${id} </p>
-                  <p>Ihr best&auml;tigter Testtermin / Your confirmed test date:</p>
-                  <p> ${req.body.date}, ${req.body.Uhrzeit} Uhr</p>
-                  <p>Ort / Location: Teststation : Gewerbepark (Antigen-Schnelltest) - Am See - 93059 - Regensburg</p>
-                  <p>Wir bitten darum, zum vereinbarten Termin p&uuml;nktlich an der Station zu erscheinen.</p>
-                  <p>Um den Test durchzuf&uuml;hren ben&ouml;tigen wir ein amtliches Ausweisdokument und Ihre Krankenversicherungskarte. Bei Kindern ohne Ausweisdokument gen&uuml;gt die Krankenversicherungskarte.</p>
-                  <p>We kindly ask you to arrive at the test station on time at the above-mentioned appointment. In order to do the test, we need to match your photo ID and health insurance card with the registration. For children, only the health insurance card is required.</p>
-                  <p>Sie erhalten Ihr Antigen-Schnelltest Ergebnis per Email bereits nach ca.15 Min. Im positiven Fall werden Ihre Daten direkt an das f&uuml;r Sie zust&auml;ndige Gesundheitsamt weitergeleitet (Gesetzesvorgabe gem&auml;&szlig; IfSG &sect;6) und Sie m&uuml;ssen sich sofort in h&auml;usliche Quarant&auml;ne begeben. Ein PCR Test ist zus&auml;tzlich notwendig &ndash; bitte wenden Sie sich hierf&uuml;r an Ihren Hausarzt. Das Gesundheitsamt wird mit Ihnen Kontakt aufnehmen und das weitere Vorgehen besprechen.</p>`,
+                <p>Sehr geehrte Frau/Herr ${req.body.Nachname} , <br />Ihr Termin wurde bei uns erfolgreich gebucht.<br />Um den Test durchzuf&uuml;hren, brauchen wir ein amtliches Ausweisdokument und ihre Versichertenkarte.<br />Bei Kindern ohne Ausweisdokument reicht die Versichertenkarte aus. <br /><strong>Sie k&ouml;nnen den enthaltenden QR-Code jederzeit bei uns wiederverwenden.</strong></p>
+                <p>Ihr ID:</p>
+                <p>${id}</p>
+                <p>Ihr Termin :&nbsp;</p>
+                <p>${req.body.date}</p>
+                <p>Teststation:&nbsp; Rennweg 5, 93049 Regensburg</p>
+                <p>Dear Madam/sir&nbsp;${req.body.Nachname} ,<br />your date is checked in successfully.<br />In order to do the test, we need to check your photo ID and health insurance card with the registration. For children the health insurance card is accepted.<br />Please note the code within this e-mail may be used once a day.</p>
+                <p>Im positiven Fall werden Ihre Daten direkt an das f&uuml;r Sie zust&auml;ndige Gesundheitsamt weitergeleitet (Gesetzesvorgabe gem&auml;&szlig; IfSG &sect;6) und Sie m&uuml;ssen sich sofort in h&auml;usliche Quarant&auml;ne begeben. Ein PCR Test ist zus&auml;tzlich notwendig &ndash; bitte wenden Sie sich hierf&uuml;r an Ihren Hausarzt. Das Gesundheitsamt wird mit Ihnen Kontakt aufnehmen und das weitere Vorgehen besprechen.</p>
+                <p>Haben Sie Fragen? Gerne k&ouml;nnen Sie sich telefonisch bei unserem Personal melden <br />+123455 <br />If something is not clear yet, feel free to contact us<br />+12356</p>
+                
+                `,
                 attachments: [   {   // data uri as an attachment
                     filename: `${id}_QrCOde.png`,
                     path: code
