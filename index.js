0059037b4e6147df8b58f6a3e3ec24212a910bdd
Salman <78206653+samofxi@users.noreply.github.com>
2022-01-17 14:05:36 +0100

    update data

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ const cors = require('cors');
 const connectDB = require('./database/connection');
 const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
-const {check, validationResult}= require('express-validator');
+const { check, validationResult } = require('express-validator');
 const path = require('path');
+const rateLimit = require('express-rate-limit')
 
+const createAccountLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hour
+    max: 10, // Limit each IP to 5 create account requests per `window` (here, per hour)
+    message:
+        'von diesem Geräte dürfen keine weitere Termine erstellt werden, da Sie Ihren Limit erreicht haben. Versuchen Sie es nach einer Stunde wieder oder verwenden Sie ein anderes Gerät!',
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 const PORT = process.env.PORT || 8080;
 connectDB();
 const app = express();
 app.set('view engine', 'ejs');
-const urlencodeParser = bodyParser.urlencoded({extended:false});
+app.use(createAccountLimiter);
+const urlencodeParser = bodyParser.urlencoded({ extended: false });
 app.use(cors());
 app.use(express.json());
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
@@ -19,5 +29,5 @@ app.use('/img', express.static(path.resolve(__dirname, "assets/img")));
 app.use('/css-1', express.static(path.resolve(__dirname, "views")));
 app.use('/', require('./routes/router'));
 
-app.listen(PORT, () => console.log('node Started on 5000'));
+app.listen(PORT, () => console.log(`node Started on ${PORT}`));
 
